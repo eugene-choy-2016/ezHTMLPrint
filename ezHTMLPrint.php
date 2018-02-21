1bf71554bab90818672f4186bc7468aed2fc57ed
Eugene Choy <eugene.choy.2016@sis.smu.edu.sg>
2018-02-21 23:09:57 +0800

    +Added printing of HTML tags +Ensure HTML Tag is balanced with htmlTagBalanced() function +Added a function that printErrorMessages for EzHTMLPrint related errors +Rename print_h to header instead

--- ezHTMLPrint.php
+++ ezHTMLPrint.php
@@ -6,8 +6,57 @@
  */
 
 class ezHTMLPrint{
-    public static function printH($headerNumber,$text){
+
+    public static $htmlTagBalance = 0;
+
+    /**
+     * Start of HTML tag
+     * Adds 1 to HTML tag balance
+     */
+    public static function html_s(){
+        echo "<html>";
+        self::$htmlTagBalance += 1;
+    }
+
+    /**
+     * End of HTML Tag
+     * Minus 1 to HTML Tag balance
+     */
+    public static function html_e(){
+        echo "</html>";
+        self::$htmlTagBalance -= 1;
+    }
+
+    /**
+     * Check if a HTML Tag is balanced if it is not balance echo out a error message and return false
+     */
+    public static function htmlTagBalanced(){
+        if(self::$htmlTagBalance == 0){
+            return true;
+        }
+
+        printEzHtmlPrintErrorMessage("HTML Tag not balanced. Please check");
+        return false; 
+    }
+
+    /**
+     * Print Header
+     * Pass in header number and text
+     */
+    public static function header($headerNumber,$text){
         echo "<h$headerNumber>$text</h$headerNumber>";
+    }
+
+    /**
+     * Print Paragraph with text
+     * Pass in text
+     */
+    public static function para($text){
+        echo "<p>$text</p>";
+    }
+
+    function printEzHtmlPrintErrorMessage($message){
+        echo "ezHTMLPrint::$message";
     }
 }
 
--- test.php
+++ test.php
@@ -8,6 +8,8 @@
     ezHTMLPrint::printH(1,"Header 1 test");
     ezHTMLPrint::printH(2,"Header 2 test");
     ezHTMLPrint::printH(3,"header 3 test");
+
+    ezHTMLPrint::html_s();
 ?>
     testing here
 </body>
